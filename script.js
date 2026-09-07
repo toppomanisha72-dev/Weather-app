c8f621ffd07159e187078a8d9dac183f79fd79ca
toppomanisha72-dev <toppomanisha72@gmail.com>
2026-09-07 14:24:57 +0530

    Fetch weather data on city search
    Updated the search button event listener to fetch weather data based on user input city. Added error handling for city not found and updated DOM elements with fetched weather information.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const input = document.getElementById("cityInput");
 const searchBtn = document.getElementById("searchBtn");
 
-searchBtn.addEventListener("click", function () {
+searchBtn.addEventListener("click", async function () {
   const city = input.value.trim();
 
   if (city === "") {
@@ -9,7 +9,30 @@ searchBtn.addEventListener("click", function () {
     return;
   }
 
-  document.getElementById("city").textContent = city;
-  document.getElementById("temperature").textContent = "25°C";
-  document.getElementById("description").textContent = "Weather available soon 🌤️";
+  const locationResponse = await fetch(
+    `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(city)}&count=1`
+  );
+
+  const locationData = await locationResponse.json();
+
+  if (!locationData.results) {
+    alert("City not found");
+    return;
+  }
+
+  const latitude = locationData.results[0].latitude;
+  const longitude = locationData.results[0].longitude;
+  const cityName = locationData.results[0].name;
+
+  const weatherResponse = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weather_code`
+  );
+
+  const weatherData = await weatherResponse.json();
+
+  document.getElementById("city").textContent = cityName;
+  document.getElementById("temperature").textContent =
+    weatherData.current.temperature_2m + "°C";
+  document.getElementById("description").textContent =
+    "Current weather 🌤️";
 });
