9ebcfe14b66c1734f25405e9d2188bce1ef2e24e
toppomanisha72-dev <toppomanisha72@gmail.com>
2026-09-14 15:50:24 +0530

    Modify weather API request parameters
    Updated weather API URL to include apparent temperature.

--- script.js
+++ script.js
@@ -34,8 +34,8 @@ searchBtn.addEventListener("click", async function () {
     const cityName = locationData.results[0].name;
 
     const weatherResponse = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weather_code,relative_humidity_2m,wind_speed_10m`
-    );
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}
+   current=temperature_2m,apparent_temperature,weather_code,relative_humidity_2m,wind_speed_10m );
 
     const weatherData = await weatherResponse.json();
 
